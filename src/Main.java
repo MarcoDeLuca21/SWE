ebb268f5be1aae5bd092b47e4c50c8e9f8607ff7
MarcoDeLuca21 <marco.deluca4@stud.unifi.if>
2023-11-03 17:15:41 +0100

    piccola correzzione

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         Warehouse warehouse = new Warehouse();
-
         FactoryProduct factory = new FactoryProduct();
 
         Date date = new Date(System.currentTimeMillis());
 
-        Product prodotto1 = factory.createFrozenProduct("A",25.50,10,date);
-        Product prodotto2 = factory.createVeggyProduct("B",15.99,15,date);
+        Product prodotto1 = factory.createFrozenProduct("A",20.23,10,date);
+        Product prodotto2 = factory.createVeggyProduct("B",10.99,12,date);
+
+        prodotto1.viewInfo();
+        prodotto2.viewInfo();
 
         Manager manager = Manager.getInstance("Mario","Rossi",4,warehouse);
         Employee employee1 = new Employee("marco","de luca",1, manager,warehouse);
@@ -20,15 +24,88 @@ public class Main {
         manager.addEmployee(employee2);
         manager.addEmployee(employee3);
 
-        employee2.addProduct(prodotto1);
+        manager.notify(employee1,"Forse funziona");
+
+        warehouse.addProduct(prodotto1);
+        warehouse.addProduct(prodotto2);
         employee1.addProduct(prodotto2);
 
+        warehouse.viewProducts();
+
+        System.out.println("vedi se funziona");
+        System.out.println();
+
+        employee1.modifyPrice(prodotto1,50.99);
+
+        System.out.println();
+        System.out.println();
+
+        employee2.modifyQuantity(prodotto2, 32);
+
+        employee2.modifyPrice(prodotto1, 21.01);
+
+        System.out.println();
+
+        manager.viewAllProducts();
+
+        employee1.addProduct(prodotto1);
+        employee1.modifyQuantity(prodotto1,26);
+        employee2.addProduct(prodotto1);
+        employee2.addProduct(prodotto2);
+        employee1.modifyQuantity(prodotto1,1);
+
+        warehouse.viewProducts();
+
+        System.out.println();
+        manager.viewAllEmployees();
+
+        System.out.println();
+        System.out.println("prova riserva scorte");
+        System.out.println();
+
+        employee2.modifyQuantity(prodotto1,0);
+        employee2.modifyQuantity(prodotto2,10);
+        employee1.modifyQuantity(prodotto1,0);
+        employee1.notifyProductOutOfStock();
+        System.out.println();
+
+        manager.setWeekSchedule(employee1, DayOfWeek.MONDAY, new DailySchedule(LocalTime.of(12,30,00),LocalTime.of(18,30,00)));
+        manager.setWeekSchedule(employee1, DayOfWeek.THURSDAY, new DailySchedule(LocalTime.of(15,30,00),LocalTime.of(21,30,00)));
+        manager.setWeekSchedule(employee1, DayOfWeek.FRIDAY, new DailySchedule(LocalTime.of(6,30,00),LocalTime.of(12,30,00)));
+
+        manager.setWeekSchedule(employee2, DayOfWeek.MONDAY, new DailySchedule(LocalTime.of(6,00,00),LocalTime.of(13,00,00)));
+        manager.setWeekSchedule(employee2, DayOfWeek.TUESDAY, new DailySchedule(LocalTime.of(12,30,00),LocalTime.of(18,30,00)));
+        manager.setWeekSchedule(employee2, DayOfWeek.WEDNESDAY, new DailySchedule(LocalTime.of(14,30,00),LocalTime.of(22,30,00)));
+
+
+        String scheduleEmployee1 = employee1.viewWeekSchedule();
+        String scheduleEmployee2 = employee2.viewWeekSchedule();
+        System.out.println("orario "+employee1.toString()+" : \n"+scheduleEmployee1);
+        System.out.println("orario "+employee2.toString()+" : \n"+scheduleEmployee2);
+
+        manager.viewAllProducts();
+        System.out.println();
+
+        System.out.println("prova prodotti per tipo");
+
+        System.out.println();
+
+        manager.viewProductsByType(prodotto2.getClass().getSimpleName());
+        System.out.println();
         manager.viewProductsByType(prodotto1.getClass().getSimpleName());
 
-        employee3.viewProductsByType(prodotto2.getClass().getSimpleName());
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("prova scadenza prodotti");
 
-        employee3.modifyPrice(prodotto1,30);
-
-        manager.notifyAllEmployee("Ciao come state");
+        Date oggi = new Date();
+        Date datascadenza = new Date(oggi.getTime()+(24 * 60 * 60 * 1000));
+        Date secondadata = new Date(oggi.getTime()-(24 * 60 * 60 * 1000));
+        prodotto2.setExpiryDate(datascadenza);
+        employee2.removeProduct(prodotto2);
+        prodotto1.setExpiryDate(secondadata);
+        employee1.notifyExpiredProduct(prodotto1.getClass().getSimpleName());
+        employee1.notifyExpiredProduct(prodotto2.getClass().getSimpleName());
     }
 }
